920fdfa1871961cbbf41b60656c4f716f49627cc
abzen <abovezenith@gmail.com>
2021-04-20 22:04:47 +0100

    mint form

--- src/components/create/page-create-single.tsx
+++ src/components/create/page-create-single.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PageContainer from 'components/common/page-container';
 import YourCollections from 'components/create/your-collections';
+import MintNftForm from 'components/create/forms/mint-nft-form';
 
 const PageCreateSingle = () => (
   <PageContainer data-name="mint-page">
+    <MintNftForm />
     <YourCollections />
   </PageContainer>
 );
