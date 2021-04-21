fbf9aa9544bf14bf3ae7e78eb15045e0a83c5a74
Yuri <yuripetusko@gmail.com>
2021-04-21 20:15:24 +0100

    Create locales.ts

---
+++ src/lib/common/locales.ts
@@ -0,0 +1,17 @@
+export type TLocales = Record<string, { value: string; text: string }>;
+
+export const locales: TLocales = {
+  en: {
+    value: 'en',
+    text: 'Eng',
+  },
+  // ru: {
+  //   value: 'ru',
+  //   text: 'Rus',
+  // },
+  it: {
+    value: 'it',
+    text: 'It',
+  },
+};
+
