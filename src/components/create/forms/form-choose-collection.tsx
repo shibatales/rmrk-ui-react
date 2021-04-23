ec4cdf030ad611b3fd2b1b6cc276684d77aa9045
abzen <abovezenith@gmail.com>
2021-04-23 13:46:11 +0100

    split radios

---
+++ src/components/create/forms/form-choose-collection.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Box, SimpleGrid, useRadioGroup, UseFormMethods } from '@chakra-ui/react';
+import CreateCollectionCard from 'components/create/create-collection/create-collection-card';
+import Label from 'components/common/inputs/label';
+import RadioCard from 'components/common/inputs/radio-card';
+
+interface IProps {
+  register: UseFormMethods.register;
+}
+
+const collections: { label: string; src: string; value: string | number }[] = [
+  {
+    label: 'Collection 1',
+    src: 'https://cryptologos.cc/logos/polkadot-new-dot-logo.png',
+    value: '1',
+  },
+  {
+    label: 'Collection 2',
+    src:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc9m6wTQLJVsy8DPDft5Z38YDGMmiVGhd9sVcCX1wHWFRitMdK23SeEBLhwqO8YlI9MHk&usqp=CAU',
+    value: '2',
+  },
+];
+
+const FormChooseCollection = ({ register }: IProps) => {
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'collection',
+    defaultValue: '1',
+    onChange: () => console.log('wow'),
+  });
+
+  const group = getRootProps();
+
+  return (
+    <Box data-name="form-collections">
+      <Box mb={2}>
+        <Label>Choose collection</Label>
+      </Box>
+      <SimpleGrid columns={3} spacing={6} {...group}>
+        <CreateCollectionCard />
+        {collections.map(({ value, label, src }) => {
+          const radio = getRadioProps({ value });
+
+          return <RadioCard {...radio} key={value} label={label} img={src} ref={register} />;
+        })}
+      </SimpleGrid>
+    </Box>
+  );
+};
+
+export default FormChooseCollection;
--- src/components/create/forms/mint-nft-form.tsx
+++ src/components/create/forms/mint-nft-form.tsx
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button, SimpleGrid, useRadioGroup, useToast } from '@chakra-ui/react';
+import { Box, Button, useToast } from '@chakra-ui/react';
 import FormHeading from 'components/create/forms/form-heading';
 import { useForm } from 'react-hook-form';
 import Input from 'components/common/inputs/input';
 import Textarea from 'components/common/inputs/textarea';
 import { IMintFormField } from 'lib/types';
 import Dropzone from 'components/common/dropzone';
-import FormChooseCollections from 'components/create/form-choose-collection';
+import FormChooseCollection from 'components/create/forms/form-choose-collection';
 import { useTranslation } from 'next-i18next';
 import { useTransactionStatus } from 'lib/nft/transaction-status';
-import Label from 'components/common/inputs/label';
-import CreateCollectionCard from 'components/create/create-collection/create-collection-card';
-import RadioCard from 'components/common/inputs/radio-card';
 
 const MintCollectionForm = () => {
   const { t } = useTranslation('page-create');
@@ -70,26 +67,6 @@ const MintCollectionForm = () => {
     },
   ];
 
-  const collections: { label: string; src: string; value: string | number }[] = [
-    {
-      label: 'Collection 1',
-      src: 'https://cryptologos.cc/logos/polkadot-new-dot-logo.png',
-      value: '1',
-    },
-    {
-      label: 'Collection 2',
-      src:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc9m6wTQLJVsy8DPDft5Z38YDGMmiVGhd9sVcCX1wHWFRitMdK23SeEBLhwqO8YlI9MHk&usqp=CAU',
-      value: '2',
-    },
-  ];
-
-  const { getRootProps, getRadioProps } = useRadioGroup({
-    name: 'collection',
-    defaultValue: '1',
-    onChange: () => console.log('wow'),
-  });
-
   const onSubmit = (data: any) => {
     if (!formFile) {
       transactionStatus.warning('Please upload some file');
@@ -97,8 +74,6 @@ const MintCollectionForm = () => {
     // const ipfsUrl = await pinMetadataFile(formFile)
     console.log(data);
   };
-
-  const group = getRootProps();
 
   return (
     <Box data-name="mint-nft-form">
@@ -110,17 +85,7 @@ const MintCollectionForm = () => {
           <Dropzone setFormFile={setFormFile} />
         </Box>
         <Box mb={4}>
-          <Box mb={2}>
-            <Label>Choose collection</Label>
-          </Box>
-          <SimpleGrid columns={3} spacing={6} {...group}>
-            <CreateCollectionCard />
-            {collections.map(({ value, label, src }) => {
-              const radio = getRadioProps({ value });
-
-              return <RadioCard {...radio} key={value} label={label} img={src} ref={register} />;
-            })}
-          </SimpleGrid>
+          <FormChooseCollection register={register} />
         </Box>
         {formFieldList.map((item, i) => (
           <Box mt={i === 0 ? undefined : 4} key={`mint-nft-form-field-${item.name}`}>
